745be61a7d3c5ec47ccc32a25779f46712a70d87
pavallasivamani <113960921+pavallasivamani@users.noreply.github.com>
2022-10-03 11:47:06 +0530

    Add files via upload

---
+++ cd 9.cpp
@@ -0,0 +1,15 @@
+#define PI 3.14
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int a,b,c = 30;	
+ printf("hello");
+ }#include<stdio.h>
+int main()
+{
+ int a,b = 20, c = 30;	
+ printf("hello");
+ }
+Footer
+© 2022 GitHub, Inc.
